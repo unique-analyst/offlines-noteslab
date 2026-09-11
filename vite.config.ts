11290669956ea1e8e74d70dbe8a753096ca18b36
unique-analyst <akintundefawaz545@gmail.com>
2026-09-11 22:42:01 +0100

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/offlines-notes-lab/',
+  base: '/offlines-noteslab/',
   plugins: [react()],
 })
